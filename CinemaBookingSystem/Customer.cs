e3375eb0d3bcd77031259f099622d1a9ab4b49d0
andri.joos69 <andri.joos.tbz@gmail.com>
2018-06-08 09:22:25 +0200

    aj:

--- CinemaBookingSystem/Customer.cs
+++ CinemaBookingSystem/Customer.cs
@@ -13,16 +13,18 @@ namespace CinemaBookingSystem
         internal Seat Seat;
         internal Show Show;
 
-        public Customer(Seat seat, Show film, string name = null, string prename = null)
+        public Customer(Seat seat, Show show, string name = null, string prename = null)
         {
             this.Name = name;
             this.Prename = prename;
             this.Seat = seat;
-            this.Show = film;
+            this.Show = show;
 
-            Seat.IsBooked.Find().
+            Seat.IsBooked[Seat.IsBooked.IndexOf(new Tuple<Show, bool>(show, false))] = null;
 
-            Seat.IsBooked.Add(new Tuple<Show, bool>(film, true));
+            var t = Show.ListOfShows[0].ShowRoom.ListOfSeats[0].IsBooked[0].Item1;
+
+            Seat.IsBooked.Add(new Tuple<Show, bool>(show, true));
 
         }
     }
