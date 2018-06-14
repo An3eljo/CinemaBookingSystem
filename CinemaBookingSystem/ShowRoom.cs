2b7f9e71671554df27206de2e532716d0e0299d0
An3eljo <37050918+An3eljo@users.noreply.github.com>
2018-06-14 08:05:14 +0200

    Update ShowRoom.cs

--- CinemaBookingSystem/ShowRoom.cs
+++ CinemaBookingSystem/ShowRoom.cs
@@ -9,11 +9,15 @@ namespace CinemaBookingSystem
     class ShowRoom
     {
         internal int RoomNumber;
+        internal int RowsCount;
+        internal int ColumnsCount;
         internal List<Seat> ListOfSeats = new List<Seat>();
 
         public ShowRoom(int roomNumber, int rows, int columns)
         {
             this.RoomNumber = roomNumber;
+            this.RowsCount = rows;
+            this.ColumnsCount = columns;
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
