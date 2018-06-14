8e419dbdfc8d93472d7a7550c7c8557f4e58446b
An3eljo <37050918+An3eljo@users.noreply.github.com>
2018-06-14 08:02:40 +0200

    Update ShowRooms.cs

--- CinemaBookingSystem/ShowRooms.cs
+++ CinemaBookingSystem/ShowRooms.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//todo: Make ShowRoom.cs Singleton => No ShowRooms.cs
 namespace CinemaBookingSystem
 {
     static class ShowRooms
