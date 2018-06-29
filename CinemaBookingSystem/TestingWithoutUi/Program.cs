dc4c3751ce8a9aa326f180ea43a4ea74f3fc55ba
andri.joos69 <andri.joos.tbz@gmail.com>
2018-06-29 11:39:46 +0200

    disabling warning new

--- CinemaBookingSystem/TestingWithoutUi/Program.cs
+++ CinemaBookingSystem/TestingWithoutUi/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using CinemaBookingSystem;
 using CinemaBookingSystem.Model;
 
 namespace TestingWithoutUi
@@ -13,70 +8,7 @@ namespace TestingWithoutUi
         
         static void Main(string[] args)
         {
-            //var printlistone = new List<string>();
-            //printlistone.AddRange(new []
-            //{
-            //    "booking",
-            //    "film",
-            //    "show"
-            //});
-            //Console.WriteLine();
-            //Console.WriteLine();
-            //var firstinput = PrintOut(printlistone, true);
-            //string secondInput = null;
-            //switch (firstinput)
-            //{
-            //    case "0":
-            //        var printlistsecond = new List<string>();
-            //        printlistsecond.AddRange(new []
-            //        {
-            //            "new booking",
-            //            "manage booking"
-            //        });
-            //        secondInput = PrintOut(printlistsecond, true);
-            //        break;
-
-            //    case "1":
-                    
-            //        break;
-
-            //    case "2":
-            //        break;
-            //}
-
-            //switch (secondInput)
-            //{
-            //    case "0":
-            //        switch (firstinput)
-            //        {
-            //            case "0":
-            //                Console.WriteLine();
-            //                CreateFilm();
-            //        }
-            //        break;
-            //}
-
-            //string PrintOut(List<string> printList, bool shouldread = false)
-            //{
-            //    for (int i = 0; i < printList.Count; i++)
-            //    {
-            //        Console.WriteLine(i + @".  " + printList[i] + @":");
-            //    }
-
-            //    if (shouldread)
-            //    {
-            //        return Console.ReadLine();
-            //    }
-
-            //    return null;
-            //}
-
-            //List<T> AddToList<T>(List<T> list, T item)
-            //{
-            //    list.Add(item);
-            //    return list;
-            //}
-
+            // ReSharper disable once ObjectCreationAsStatement
             new ShowRoom(0, 6, 7);
 
             CreateFilm();
@@ -87,6 +19,7 @@ namespace TestingWithoutUi
 
         static void CreateFilm()
         {
+            // ReSharper disable once ObjectCreationAsStatement
             new Film("newFilm", new TimeSpan(0, 5, 2, 3));
             var t = Film.ListOfFilms;
         }
@@ -95,6 +28,7 @@ namespace TestingWithoutUi
 
         static void CreateShow()
         {
+            // ReSharper disable once ObjectCreationAsStatement
             new Show(Film.ListOfFilms[0], DateTime.UtcNow, ShowRoom.ShowRooms[0]);
             var t = Show.ListOfShows;
         }
