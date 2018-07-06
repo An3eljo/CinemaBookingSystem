111891a394530b214c9b480c039d0ae1bad6adf5
Adrian.Flueckiger <adi.flueckiger.11@gmail.com>
2018-07-06 09:53:10 +0200

    af

--- CinemaBookingSystem/MainWindow.xaml.cs
+++ CinemaBookingSystem/MainWindow.xaml.cs
@@ -13,7 +13,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using CinemaBookingSystem.Model;
-using CinemaBookingSystem.View.Booking;
 
 namespace CinemaBookingSystem
 {
