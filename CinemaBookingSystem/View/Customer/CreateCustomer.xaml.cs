4dba6a62c049bf997d5fdd0790d7573ee1fdd060
Joos, Andri <Andri.Joos@trumpf.com>
2018-07-12 17:00:25 +0200

    fixing CreateCustomer

--- CinemaBookingSystem/View/Customer/CreateCustomer.xaml.cs
+++ CinemaBookingSystem/View/Customer/CreateCustomer.xaml.cs
@@ -10,42 +10,23 @@ namespace CinemaBookingSystem.View.Customer
     /// </summary>
     public partial class CreateCustomer : Page
     {
-        //private Model.Customer CurrentCustomer;
-        public CreateCustomer(Model.Customer customer = null)
+        public CreateCustomer()
         {
             InitializeComponent();
-            //CurrentCustomer = customer;
-            Init(customer);
+            Init();
         }
 
-        private void Init(Model.Customer customer = null)
+        private void Init()
         {
             var shows = Model.Show.ListOfShows;
             foreach (var show in shows)
             {
                 ComboBoxShow.Items.Add(show.Date.ToString() + ": " + show.Film.Title);
             }
-
-            if (customer != null)
-            {
-                var index = shows.IndexOf(customer.Show);
-                FillUi(customer, shows[index]);
-                ComboBoxShow.SelectedIndex = index;
-            }
-            else
-            {
-
-            }
         }
 
-        private void FillUi(Model.Customer customer, Model.Show show)
+        private void FillUi(Model.Show show)
         {
-            if (customer != null)
-            {
-                TextBoxPrename.Text = customer.Prename;
-                TextBoxName.Text = customer.Name;
-            }
-
             if (show != null)
             {
                 TextBlockShowFilmTitle.Text = show.Film.Title;
@@ -57,7 +38,7 @@ namespace CinemaBookingSystem.View.Customer
         private void ComboBoxShow_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             var show = Model.Show.ListOfShows[((ComboBox)sender).SelectedIndex];
-            FillUi(null, show);
+            FillUi(show);
         }
 
         private void ButtonSelectSeat_Click(object sender, RoutedEventArgs e)
